2bca605ebc23bf950dbad23b4dda6a523ef4f37b
JakeVelicer <007velicer@gmail.com>
2023-05-05 03:24:16 -0500

    Fixed Nav for Blog Pages

--- js/NavBarHandlerInProjects.js
+++
@@ -1,5 +0,0 @@
-fetch('../nav-bar-in-projects.html')
-.then(response => response.text())
-.then(data => {
-  if (document.getElementById("NavBarProjects") !== null) document.getElementById("NavBarProjects").innerHTML = data;
-});
--- js/ProjectLevelFooter.js
+++
@@ -1,5 +0,0 @@
-fetch('../footer-in-projects.html')
-.then(response => response.text())
-.then(data => {
-  document.getElementById("Footer").innerHTML = data;
-});

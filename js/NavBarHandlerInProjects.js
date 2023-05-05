0ba6d1b353581e9e94b9dc5b145bab98373a2235
JakeVelicer <007velicer@gmail.com>
2023-05-05 03:12:41 -0500

    Fixed Loading Error

--- js/NavBarHandlerInProjects.js
+++ js/NavBarHandlerInProjects.js
@@ -1,5 +1,5 @@
 fetch('../nav-bar-in-projects.html')
 .then(response => response.text())
 .then(data => {
-  if (document.getElementById("NavBarFeatured") !== null) document.getElementById("NavBarProjects").innerHTML = data;
+  if (document.getElementById("NavBarProjects") !== null) document.getElementById("NavBarProjects").innerHTML = data;
 });

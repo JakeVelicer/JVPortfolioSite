a5ebf2d4d16b0e1e8d17960e0eb9d37f133478d6
JakeVelicer <007velicer@gmail.com>
2023-05-05 02:28:40 -0500

    Navigation Loads Through JS

--- js/main.js
+++ js/main.js
@@ -3,28 +3,44 @@ let burgerMenu = document.querySelector(".MobileNavMenu");
 let masthead = document.querySelector(".MastheadImage");
 let navbar = document.querySelector(".NavBarHeader");
 
-let isBurgerOpen = false;
-
-burgerBtn.onclick = function ()
+var checkExist = setInterval(function()
 {
-    if (!isBurgerOpen)
+    burgerBtn = document.querySelector(".BurgerMenuButton");
+    burgerMenu = document.querySelector(".MobileNavMenu");
+    masthead = document.querySelector(".MastheadImage");
+    navbar = document.querySelector(".NavBarHeader");
+
+    if (burgerBtn !== null && burgerMenu !== null && masthead !== null && navbar !== null)
     {
-        burgerMenu.style.display = "block";
-        burgerBtn.style.backgroundPosition = "center left 50px, center";
-        navbar.style.position = "fixed";
-        //navbar.style.top = "240px";
-        //masthead.style.position = "fixed";
-        //masthead.style.top = "0px";
-        isBurgerOpen = true;
+        clearInterval(checkExist);
+        let isBurgerOpen = false;
+        
+        burgerBtn.onclick = function()
+        {
+            if (!isBurgerOpen)
+            {
+                burgerMenu.style.display = "block";
+                burgerBtn.style.backgroundPosition = "center left 50px, center";
+                navbar.style.position = "fixed";
+                //navbar.style.top = "240px";
+                //masthead.style.position = "fixed";
+                //masthead.style.top = "0px";
+                isBurgerOpen = true;
+            }
+            else if (isBurgerOpen)
+            {
+                burgerMenu.style.display = "none";
+                burgerBtn.style.backgroundPosition = "center, center left 50px";
+                navbar.style.position = "sticky";
+                //navbar.style.top = "0px";
+                //masthead.style.position = "static";
+                //masthead.style.top = "0px";
+                isBurgerOpen = false;
+            }
+        }
     }
-    else if (isBurgerOpen)
+    else
     {
-        burgerMenu.style.display = "none";
-        burgerBtn.style.backgroundPosition = "center, center left 50px";
-        navbar.style.position = "sticky";
-        //navbar.style.top = "0px";
-        //masthead.style.position = "static";
-        //masthead.style.top = "0px";
-        isBurgerOpen = false;
+        console.log('Element not found yet...');
     }
-}
+}, 100);

cb47b6a68206de3f67f08d7b4b8ae5f1a5006c9d
JakeVelicer <007velicer@gmail.com>
2025-02-02 00:14:46 -0600

    Footer now gets the Year Automattically through JS

--- js/TopLevelFooter.js
+++ js/TopLevelFooter.js
@@ -2,4 +2,5 @@ fetch('../footer.html')
 .then(response => response.text())
 .then(data => {
   document.getElementById("Footer").innerHTML = data;
+  document.getElementById("CurrentYear").textContent = new Date().getFullYear();
 });
